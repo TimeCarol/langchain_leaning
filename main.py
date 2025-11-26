40406363d68300962c56c5f7db28ee2c82c1a418
TimeCarol <1841744771@qq.com>
2025-11-26 15:23:07 +0800

    @init-- init project

--- main.py
+++ main.py
@@ -1,8 +1,27 @@
-from langchain.messages import HumanMessage, SystemMessage
+import os
+
+from dotenv import load_dotenv
+from langchain.chat_models import init_chat_model
+from langchain_core.messages import HumanMessage, SystemMessage, AIMessage
 
 if __name__ == '__main__':
+    load_dotenv()
+    llm = init_chat_model(
+        model='gemma-3-27b-it',
+        model_provider='openai',
+        api_key=os.getenv('OPENAI_API_KEY'),
+        base_url=os.getenv('OPENAI_BASE_URL'),
+    )
     messages = [
-        SystemMessage('你是一位乐于助人的智能小助手'),
-        HumanMessage('你好, 请介绍一下你自己')
+        SystemMessage(content='你是一个擅长人工智能相关学科的专家'),
     ]
-    print(messages)
+    while True:
+        input_msg = HumanMessage(content=input('INPUT: '))
+        messages.append(input_msg)
+        print('OUTPUT: ', end='', flush=True)
+        ai_message_content = ''
+        for chunk in llm.stream(input=messages):
+            print(chunk.content, end='', flush=True)
+            ai_message_content += chunk.content
+        ai_message = AIMessage(content=ai_message_content)
+        messages.append(ai_message)
